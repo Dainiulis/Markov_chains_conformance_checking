e9d6e6f8f6fe333339a34a753b54251f9eee005c
Dainius <dainiusmie@gmail.com>
2020-03-24 18:34:48 +0200

    Updated

--- logs_parsing/log_loader.py
+++ logs_parsing/log_loader.py
@@ -28,12 +28,12 @@ def read_uipath_log_file_as_df(data, with_faulted_cases = False, all_cases = Fal
         faulted_jobIds = df.loc[df["level"] == "Fatal", "jobId"].unique()
         faulted_cases_rows = df["jobId"].isin(faulted_jobIds)
         mask = (df["State"] == "Executing") & ~faulted_cases_rows & mask
-    df = df[mask]
+    df = df[mask].copy()
     df["ActivityName"] = df["DisplayName"] + "|" + df["State"] + "|" + df["fileName"]
 
     df.reset_index(inplace=True, drop=True)
     ft = time.process_time()
-    df["timeStamp_datetime"] = pd.to_datetime(df["timeStamp"])
+    df["timeStamp_datetime"] = pd.to_datetime(df["timeStamp"].str.replace("\+02:00", ""))
     df.rename(columns={
         "processName": Columns.PROCESS_NAME.value,
         "jobId": Columns.CASE_ID.value,
